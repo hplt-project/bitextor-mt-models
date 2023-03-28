ac8b348c185cee206993b5e88749b50c1c1a2dbf
Graeme Nail <graemenail.work@gmail.com>
2023-03-28 11:55:13 +0100

    Hin-Eng vocab script

---
+++ hin-eng/students/create_vocab.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -eu
+SPM_EXEC="/fs/surtr0/gnail/hplt/marian-dev/build-sigyn/spm_train"
+
+src="hi"
+trg="en"
+PREFIX="--model_prefix=model.${src}-${trg}"
+VOCAB_SIZE=32000
+
+$SPM_EXEC \
+  --bos_id=-1 \
+  --eos_id=0 \
+  --unk_id=1 \
+  ${PREFIX} \
+  --vocab_size=${VOCAB_SIZE} \
+  --input=<(sed 's/\t/\n/g' <../data/train/train.tsv) \
+  --input_sentence_size=20000000 \
+  --train_extremely_large_corpus \
+  --byte_fallback
