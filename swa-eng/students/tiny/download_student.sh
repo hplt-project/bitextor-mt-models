4e50d07ed1f533e95030bd87ee2ab145c95f8b45
Ona De Gibert Bonet <degibert@puhti-login12.bullx>
2023-04-11 10:37:05 +0300

    updated download files for students

--- swa-eng/students/tiny/download_student.sh
+++ swa-eng/students/tiny/download_student.sh
@@ -1,2 +1,2 @@
 # Download student model from Allas container
-wget https://a3s.fi/swift/v1/AUTH_2228de0d405e40238387c3e7c587a465/hplt-project/sw-en_student_tiny.zip
+wget https://a3s.fi/swift/v1/AUTH_2228de0d405e40238387c3e7c587a465/HPLT-bitextor-models/mt/sw-en_student_tiny.zip
