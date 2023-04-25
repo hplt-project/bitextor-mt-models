7cf96dab32400edc8d304cd9d4b3957a7753eaf0
Ona De Gibert Bonet <degibert@puhti-login12.bullx>
2023-04-25 11:46:48 +0300

    Updated info on sw and gl models

--- swa-eng/students/base/download_student.sh
+++ swa-eng/students/base/download_student.sh
@@ -1,2 +1,3 @@
 # Download student model from Allas container
-wget https://a3s.fi/swift/v1/AUTH_2228de0d405e40238387c3e7c587a465/HPLT-bitextor-models/mt/sw-en_student_base.zip
+wget https://a3s.fi/swift/v1/AUTH_2228de0d405e40238387c3e7c587a465/HPLT-bitextor-models/mt/sw-en_exported_base.zip
+
